44ebbc024797228838f6a9c8ffd6c59d688b18e3
Tolkata99 <Tolkata_04@abv.bg>
2020-09-25 17:47:46 +0300

    xx

--- digits/digits/Program.cs
+++ digits/digits/Program.cs
@@ -6,7 +6,17 @@ namespace digits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string integer =Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < integer.Length; i++)
+            {
+                int curentNumber = (int)char.GetNumericValue(integer[i]);
+                sum += curentNumber;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
