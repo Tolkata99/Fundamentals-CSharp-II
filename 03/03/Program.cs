fbb7912a15d8b81695f48af54d3c3a5b26971260
Tolkata99 <Tolkata_04@abv.bg>
2020-09-26 17:41:22 +0300

    ss

---
+++ 03/03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int CapacityPeople = int.Parse(Console.ReadLine());
+
+            int couter = 0;
+
+            while (true)
+            {
+                if(people <= 0)
+                {
+                    break;
+                }
+                people -= CapacityPeople;
+                couter++;
+            }
+
+            Console.WriteLine(couter);
+
+
+
+        }   
+    }
+}
---
+++ 03/ascii/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int n1 = int.Parse(Console.ReadLine());
+
+
+            for (int i = n; i <= n1; i++)
+            {
+                char s = ((char)i);
+                Console.Write(s);
+            }
+
+        }
+    }
+}
